21ad74ccd2d011ced42d769fc0a369840d80ef66
hiroya.shimamoto <shshimamo@gmail.com>
2020-02-16 10:04:24 +0900

    Get json in asyncData

--- pages/axios.vue
+++ pages/axios.vue
@@ -2,31 +2,61 @@
     <section class="container">
         <h1>{{title}}</h1>
         <p>{{message}}</p>
-        <pre>{{html_data}}</pre>
+        <table>
+            <tr>
+                <th>User ID</th>
+                <td>{{json_data.userId}}</td>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <td>{{json_data.id}}</td>
+            </tr>
+            <tr>
+                <th>Title</th>
+                <td>{{json_data.title}}</td>
+            </tr>
+            <tr>
+                <th>Body</th>
+                <td>{{json_data.body}}</td>
+            </tr>
+        </table>
     </section>
 </template>
 
 <script>
 const axios = require('axios');
 
-let url = "/README.md";
+let url = "https://jsonplaceholder.typicode.com/posts/";
 
 export default {
     data: function(){
         return {
             title:'Axios',
+            msg:'',
             message:'axios sample.',
         };
     },
-    asyncData: async function() {
-        let result = await axios.get(url);
-        return { html_data: result.data };
+    async asyncData () {
+        let id = 1; // ●id番号
+        let result = await axios.get(url + id);
+        return { json_data: result.data };
     },
 }
 
 </script>
 
 <style>
+tr th {
+    width:150px;
+    background-color: darkblue;
+    color:white;
+    font-size:16pt;
+}
+tr td {
+    padding:5px 10px;
+    background-color:#eef;
+    font-size:14pt;
+}
 .container {
   padding: 5px 10px;
 }
